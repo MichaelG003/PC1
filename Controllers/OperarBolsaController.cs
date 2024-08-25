8eb94e764270ae12b34f87654c817da2904b0569
Michael Orihuela <mickeyorihuela00@gmail.com>
2024-08-25 07:11:30 -0500

    funcion estable

--- Controllers/OperarBolsaController.cs
+++ Controllers/OperarBolsaController.cs
@@ -23,6 +23,58 @@ namespace PC1.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Operar(OperarBolsa operadorBolsa)
+        {
+            if (ModelState.IsValid)
+            {
+                decimal comision = operadorBolsa.MontoAbonar <= 300 ? 3m : 1m;
+                decimal totalIGV = 0m;
+                decimal totalMonto = 0m;
+
+                List<InstrumentoDetalle> detalles = new List<InstrumentoDetalle>();
+
+                if (operadorBolsa.Instrumento != null)
+                {
+                    foreach (var instrumento in operadorBolsa.Instrumento)
+                    {
+                        decimal monto = 0m;
+                        if (instrumento == "S&P 500")
+                        {
+                            monto = 500m;
+                        }
+                        else if (instrumento == "Dow Jones")
+                        {
+                            monto = 300m;
+                        }
+                        else if (instrumento == "Bonos US")
+                        {
+                            monto = 120m;
+                        }
+
+                        var igv = monto * 0.18m;
+                        totalMonto += monto;
+                        totalIGV += igv;
+
+                        detalles.Add(new InstrumentoDetalle { Nombre = instrumento, IGV = igv, Monto = monto });
+                    }
+                }
+
+                decimal total = totalMonto + totalIGV + comision;
+
+                ViewBag.Detalles = detalles;
+                ViewBag.Comision = comision;
+                ViewBag.Total = total;
+
+                return View("Index");
+            }
+
+            return View(operadorBolsa);
+        }
+
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
@@ -39,9 +91,16 @@ namespace PC1.Controllers
 
         public DateTime? FecOp { get; set; }
 
-        public string? Instrumento { get; set; }
+        public List<string>? Instrumento { get; set; }
 
         public decimal? MontoAbonar { get; set;}
 
+    }
+
+    public class InstrumentoDetalle
+    {
+        public string? Nombre { get; set; }
+        public decimal? IGV { get; set; }
+        public decimal? Monto { get; set; }
     }
 }
