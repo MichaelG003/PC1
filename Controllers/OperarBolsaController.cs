308a249feed2d44345dfeaa7158acf15c3a5e6e2
Michael Orihuela <mickeyorihuela00@gmail.com>
2024-08-25 05:10:49 -0500

    controller

---
+++ Controllers/OperarBolsaController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PC1.Controllers
+{
+   
+    public class OperarBolsaController : Controller
+    {
+        private readonly ILogger<OperarBolsaController> _logger;
+
+        public OperarBolsaController(ILogger<OperarBolsaController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
